cc0da902e2920839960e24f6dcd30837f70eb0a6
AboudyKreidieh <akreidieh@gmail.com>
2019-12-02 12:32:55 -0800

    added whatever was around

--- hbaselines/ppo/common/policies.py
+++ hbaselines/ppo/common/policies.py
@@ -1,13 +1,9 @@
 import tensorflow as tf
-import gym
+import tensorflow.contrib.slim as slim
+import numpy as np
 
-from baselines.common import tf_util
-from baselines.a2c.utils import fc
-from baselines.common.distributions import make_pdtype
-from baselines.common.input import observation_placeholder, encode_observation
 from baselines.common.tf_util import adjust_shape
 from baselines.common.running_mean_std import RunningMeanStd
-from baselines.common.models import get_network_builder
 
 
 class PolicyWithValue(object):
@@ -16,13 +12,13 @@ class PolicyWithValue(object):
     """
 
     def __init__(self,
-                 env,
-                 observations,
-                 latent,
-                 estimate_q=False,
-                 vf_latent=None,
-                 sess=None,
-                 **tensors):
+                 sess,
+                 obs_ph,
+                 ac_ph,
+                 layers,
+                 act_fun,
+                 normalize_observations,
+                 layer_norm):
         """TODO
 
         Parameters
@@ -40,35 +36,67 @@ class PolicyWithValue(object):
         **tensors       tensorflow tensors for additional attributes such as state or mask
 
         """
-        self.X = observations
+        self.sess = sess
+        self.X = obs_ph
         self.state = tf.constant([])
         self.initial_state = None
-        self.__dict__.update(tensors)
 
-        vf_latent = vf_latent if vf_latent is not None else latent
+        # =================================================================== #
+        # Part 1. Pre-process inputs.                                         #
+        # =================================================================== #
 
-        vf_latent = tf.layers.flatten(vf_latent)
-        latent = tf.layers.flatten(latent)
+        # Potentially normalize the input observations and actions.
+        if normalize_observations:
+            encoded_x, self.rms = _normalize_clip_observation(obs_ph)
+        else:
+            encoded_x = obs_ph
+        encoded_x = tf.to_float(encoded_x)
 
-        # Based on the action space, will select what probability distribution
-        # type
-        self.pdtype = make_pdtype(env.action_space)
+        # Flatten the processed observation.
+        encoded_x = tf.layers.flatten(encoded_x)
 
-        self.pd, self.pi = self.pdtype.pdfromlatent(latent, init_scale=0.01)
+        # =================================================================== #
+        # Part 2. Create the actor network.                                   #
+        # =================================================================== #
+
+        with tf.variable_scope('pi', reuse=tf.AUTO_REUSE):
+            # Create the hidden layers of the actor network.
+            latent = encoded_x
+            for i, layer in enumerate(layers):
+                latent = self._layer(latent, layer, "fc{}".format(i), act_fun,
+                                     layer_norm)
+
+            # Create the mean of the actor network.
+            mean = self._layer(latent, ac_ph.shape[-1], "pi_out", None, False)
+
+            # Create the logstd of the actor network.
+            logstd = tf.get_variable(
+                name='pi_logstd',
+                shape=(1, ac_ph.shape[-1]),
+                initializer=tf.zeros_initializer()
+            )
+
+            self.pd = DiagGaussianProbabilityDistribution(mean, logstd)
 
         # Take an action
         self.action = self.pd.sample()
 
         # Calculate the neg log of our probability
         self.neglogp = self.pd.neglogp(self.action)
-        self.sess = sess or tf.get_default_session()
 
-        if estimate_q:
-            assert isinstance(env.action_space, gym.spaces.Discrete)
-            self.q = fc(vf_latent, 'q', env.action_space.n)
-            self.vf = self.q
-        else:
-            self.vf = fc(vf_latent, 'vf', 1)
+        # =================================================================== #
+        # Part 3. Create the critic network(s).                               #
+        # =================================================================== #
+
+        with tf.variable_scope("vf", reuse=tf.AUTO_REUSE):
+            # Create the hidden layers of the critic network.
+            vf_latent = encoded_x
+            for i, layer in enumerate(layers):
+                vf_latent = self._layer(vf_latent, layer, "fc{}".format(i),
+                                        act_fun, layer_norm)
+
+            # Create the output from the critic network.
+            self.vf = self._layer(vf_latent, 1, 'vf_out', None, False)
             self.vf = self.vf[:, 0]
 
     def _evaluate(self, variables, observation, **extra_feed):
@@ -82,6 +110,44 @@ class PolicyWithValue(object):
                     feed_dict[inpt] = adjust_shape(inpt, data)
 
         return sess.run(variables, feed_dict)
+
+    @staticmethod
+    def _layer(in_val, num_output, name, act_fun, layer_norm):
+        """Create a fully connected layer.
+
+        This is here to reduce code duplication.
+
+        Parameters
+        ----------
+        in_val : tf.Tensor
+            input tensor
+        num_output : int
+            number of outputs from the layer
+        name : str
+            the name of the layer
+        act_fun : tf.nn.* or None
+            activation function. None refers to a linear combination
+
+        Returns
+        -------
+        tf.Tensor
+            the output from the new layer
+        """
+        ret = tf.compat.v1.layers.dense(
+            in_val,
+            num_output,
+            name=name,
+            kernel_initializer=slim.variance_scaling_initializer(
+                factor=1.0 / 3.0,
+                mode='FAN_IN',
+                uniform=True
+            )
+        )
+        if layer_norm:
+            ret = tf.contrib.layers.layer_norm(
+                ret, center=True, scale=True)
+
+        return ret if act_fun is None else act_fun(ret)
 
     def step(self, obs, **extra_feed):
         """Compute next action(s) given the observation(s).
@@ -127,90 +193,6 @@ class PolicyWithValue(object):
         """
         return self._evaluate(self.vf, obs, *args, **kwargs)
 
-    def save(self, save_path):
-        tf_util.save_state(save_path, sess=self.sess)
-
-    def load(self, load_path):
-        tf_util.load_state(load_path, sess=self.sess)
-
-
-def build_policy(env,
-                 policy_network,
-                 value_network=None,
-                 normalize_observations=False,
-                 estimate_q=False,
-                 **policy_kwargs):
-    """TODO
-
-    :param env:
-    :param policy_network:
-    :param value_network:
-    :param normalize_observations:
-    :param estimate_q:
-    :param policy_kwargs:
-    :return:
-    """
-    if isinstance(policy_network, str):
-        network_type = policy_network
-        policy_network = get_network_builder(network_type)(**policy_kwargs)
-
-    def policy_fn(nbatch=None, nsteps=None, sess=None, observ_placeholder=None):
-        ob_space = env.observation_space
-
-        obs_ph = observ_placeholder if observ_placeholder is not None else \
-            observation_placeholder(ob_space, batch_size=nbatch)
-
-        extra_tensors = {}
-
-        if normalize_observations and obs_ph.dtype == tf.float32:
-            encoded_x, rms = _normalize_clip_observation(obs_ph)
-            extra_tensors['rms'] = rms
-        else:
-            encoded_x = obs_ph
-
-        encoded_x = encode_observation(ob_space, encoded_x)
-
-        with tf.variable_scope('pi', reuse=tf.AUTO_REUSE):
-            policy_latent = policy_network(encoded_x)
-            if isinstance(policy_latent, tuple):
-                policy_latent, recurrent_tensors = policy_latent
-
-                if recurrent_tensors is not None:
-                    # recurrent architecture, need a few more steps
-                    nenv = nbatch // nsteps
-                    assert nenv > 0, \
-                        'Bad input for recurrent policy: batch size {} ' \
-                        'smaller than nsteps {}'.format(nbatch, nsteps)
-                    policy_latent, recurrent_tensors = policy_network(
-                        encoded_x, nenv)
-                    extra_tensors.update(recurrent_tensors)
-
-        _v_net = value_network
-
-        if _v_net is None or _v_net == 'shared':
-            vf_latent = policy_latent
-        else:
-            if _v_net == 'copy':
-                _v_net = policy_network
-            else:
-                assert callable(_v_net)
-
-            with tf.variable_scope('vf', reuse=tf.AUTO_REUSE):
-                vf_latent = _v_net(encoded_x)
-
-        policy = PolicyWithValue(
-            env=env,
-            observations=obs_ph,
-            latent=policy_latent,
-            vf_latent=vf_latent,
-            sess=sess,
-            estimate_q=estimate_q,
-            **extra_tensors
-        )
-        return policy
-
-    return policy_fn
-
 
 def _normalize_clip_observation(x, clip_range=None):
     if clip_range is None:
@@ -219,3 +201,98 @@ def _normalize_clip_observation(x, clip_range=None):
     norm_x = tf.clip_by_value((x - rms.mean) / rms.std,
                               min(clip_range), max(clip_range))
     return norm_x, rms
+
+
+class DiagGaussianProbabilityDistribution(object):
+    """TODO
+    """
+
+    def __init__(self, mean, logstd):
+        """
+        Probability distributions from multivariate gaussian input
+        :param flat: ([float]) the multivariate gaussian input data
+        """
+        self.mean = mean
+        self.logstd = logstd
+        self.std = tf.exp(logstd)
+        self.flat = tf.concat([mean, mean * 0.0 + logstd], axis=1)
+
+    def flatparam(self):
+        """Return the direct probabilities."""
+        return self.flat
+
+    def mode(self):
+        """Return the probability (deterministic action)."""
+        return self.mean
+
+    def neglogp(self, x):
+        """Return the of the negative log likelihood.
+
+        Parameters
+        ----------
+        x : array_like
+            TODO
+
+        Returns
+        -------
+        array_like
+            The negative log likelihood of the distribution
+        """
+        return 0.5 * tf.reduce_sum(tf.square((x - self.mean) / self.std),
+                                   axis=-1) \
+            + 0.5 * np.log(2. * np.pi) * tf.cast(tf.shape(x)[-1], tf.float32) \
+            + tf.reduce_sum(self.logstd, axis=-1)
+
+    def kl(self, other):
+        """Calculate the Kullback-Leibler divergence of the distribution.
+
+        Parameters
+        ----------
+        other : list of float
+            the distribution to compare with
+
+        Returns
+        -------
+        float
+            the KL divergence of the two distributions
+        """
+        assert isinstance(other, DiagGaussianProbabilityDistribution)
+        return tf.reduce_sum(
+            other.logstd - self.logstd +
+            (tf.square(self.std) + tf.square(self.mean - other.mean)) /
+            (2.0 * tf.square(other.std)) - 0.5,
+            axis=-1)
+
+    def entropy(self):
+        """Return shannon's entropy of the probability."""
+        return tf.reduce_sum(self.logstd + .5 * np.log(2.0 * np.pi * np.e),
+                             axis=-1)
+
+    def sample(self):
+        """Return a sample from the probability distribution.
+
+        Note: Bounds are taken into account outside this class (during training
+        only).
+
+        Returns
+        -------
+        tf.Tensor
+            the stochastic action
+        """
+        return self.mean + self.std * tf.random_normal(tf.shape(self.mean),
+                                                       dtype=self.mean.dtype)
+
+    def logp(self, x):
+        """Return the of the log likelihood.
+
+        Parameters
+        ----------
+        x : str
+            the labels of each index
+
+        Returns
+        -------
+        list of float
+            the log likelihood of the distribution
+        """
+        return - self.neglogp(x)
--- hbaselines/ppo/policy.py
+++ hbaselines/ppo/policy.py
@@ -1,33 +1,60 @@
 import tensorflow as tf
 import functools
 
+from hbaselines.ppo.common.policies import PolicyWithValue
+# TODO: remove
 from baselines.common.tf_util import save_variables, load_variables
-from baselines.common.tf_util import initialize
 
 
-class Model(object):
-    """TODO
+class FeedForwardPolicy(object):
+    """Feed-forward policy object.
+
+    This policy is compatible with the PPO training algorithm.
 
     Attributes
     ----------
     sess : tf.compat.v1.Session
         the tensorflow session
+    obs_ph : tf.compat.v1.placeholder
+        placeholder for the observations
     ac_ph : tf.compat.v1.placeholder
         placeholder for the actions
     adv_ph : tf.compat.v1.placeholder
         placeholder for the advantages
     ret_ph : tf.compat.v1.placeholder
         placeholder for the returns
-    OLDNEGLOGPAC : tf.compat.v1.placeholder
+    old_neglogpac : tf.compat.v1.placeholder
         placeholder for the negative log-probability of actions in the previous
         step policy
-    OLDVPRED : tf.compat.v1.placeholder
+    old_vpred : tf.compat.v1.placeholder
         placeholder for the predicted values from the previous step policy
     learning_rate : tf.compat.v1.placeholder
         placeholder for the current learning rate
     clip_range : tf.compat.v1.placeholder
         placeholder for the current clip range to the gradients
+    trainer : TODO
+        TODO
+    grads : TODO
+        TODO
+    var : TODO
+        TODO
+    _train_op : tf.Operation
+        the training operation
+    loss_names : list of str
+        the names of the individual losses; used for logging purposes
+    stats_list : list of tf.Operation
+        the tensorflow operations that produce the individual elements of the
+        loss; used for logging purposes
+    train_model : TODO
+        TODO
+    act_model : TODO
+        TODO
+    step : TODO
+        TODO
+    value : TODO
+        TODO
     """
+
     def __init__(self,
                  *,
                  sess,
@@ -41,30 +68,21 @@ class Model(object):
                  vf_coef,
                  max_grad_norm,
                  microbatch_size=None):
-        """TODO
+        """Instantiate the policy object.
 
         Parameters
         ----------
-        policy : TODO
-            TODO
-        ob_space : TODO
-            TODO
-        ac_space : TODO
-            TODO
-        nbatch_act : TODO
-            TODO
-        nbatch_train : TODO
-            TODO
-        nsteps : TODO
-            TODO
-        ent_coef : TODO
-            TODO
-        vf_coef : TODO
-            TODO
-        max_grad_norm : TODO
-            TODO
-        microbatch_size : TODO
-            TODO
+        ob_space : gym.spaces.*
+            the observation space of the environment
+        ac_space : gym.spaces.*
+            the action space of the environment
+            number of training steps per iteration
+        ent_coef : float
+            policy entropy coefficient in the optimization objective
+        vf_coef : float
+            value function loss coefficient in the optimization objective
+        max_grad_norm : float or None
+            gradient norm clipping coefficient
         """
         self.sess = sess
 
@@ -72,15 +90,13 @@ class Model(object):
         # Part 1. Create the placeholders.                                    #
         # =================================================================== #
 
+        self.obs_ph = tf.placeholder(tf.float32, [None, ob_space.shape[0]])
         self.ac_ph = tf.placeholder(tf.float32, [None, ac_space.shape[0]])
         self.adv_ph = tf.placeholder(tf.float32, [None])
         self.ret_ph = tf.placeholder(tf.float32, [None])
-        # Keep track of old actor
-        self.OLDNEGLOGPAC = OLDNEGLOGPAC = tf.placeholder(tf.float32, [None])
-        # Keep track of old critic
-        self.OLDVPRED = OLDVPRED = tf.placeholder(tf.float32, [None])
+        self.old_neglogpac = tf.placeholder(tf.float32, [None])
+        self.old_vpred = tf.placeholder(tf.float32, [None])
         self.learning_rate = tf.placeholder(tf.float32, [])
-        # Cliprange
         self.clip_range = tf.placeholder(tf.float32, [])
 
         # =================================================================== #
@@ -99,7 +115,7 @@ class Model(object):
 
         # =================================================================== #
         # Part 3. Calculate the loss.                                         #
-        # =================================================================== #
+        #                                                                     #
         # Total loss = policy gradient loss - entropy * entropy coefficient   #
         #              + Value coefficient * value loss                       #
         # =================================================================== #
@@ -113,8 +129,9 @@ class Model(object):
         # Clip the value to reduce variability during Critic training
         # Get the predicted value
         vpred = train_model.vf
-        vpredclipped = OLDVPRED + tf.clip_by_value(
-            train_model.vf - OLDVPRED, - self.clip_range, self.clip_range)
+        vpredclipped = self.old_vpred + tf.clip_by_value(
+            train_model.vf - self.old_vpred,
+            - self.clip_range, self.clip_range)
         # Unclipped value
         vf_losses1 = tf.square(vpred - self.ret_ph)
         # Clipped value
@@ -123,7 +140,7 @@ class Model(object):
         vf_loss = .5 * tf.reduce_mean(tf.maximum(vf_losses1, vf_losses2))
 
         # Calculate ratio (pi current policy / pi old policy)
-        ratio = tf.exp(OLDNEGLOGPAC - neglogpac)
+        ratio = tf.exp(self.old_neglogpac - neglogpac)
 
         # Defining Loss = - J is equivalent to max J
         pg_losses = -self.adv_ph * ratio
@@ -133,7 +150,8 @@ class Model(object):
 
         # Final PG loss
         pg_loss = tf.reduce_mean(tf.maximum(pg_losses, pg_losses2))
-        approxkl = .5 * tf.reduce_mean(tf.square(neglogpac - OLDNEGLOGPAC))
+        approxkl = .5 * tf.reduce_mean(
+            tf.square(neglogpac - self.old_neglogpac))
         clipfrac = tf.reduce_mean(
             tf.to_float(tf.greater(tf.abs(ratio - 1.0), self.clip_range)))
 
@@ -158,9 +176,9 @@ class Model(object):
         if max_grad_norm is not None:
             # Clip the gradients (normalize)
             grads, _grad_norm = tf.clip_by_global_norm(grads, max_grad_norm)
-        grads_and_var = list(zip(grads, var))
         # zip aggregate each gradient with parameters associated
         # For instance zip(ABCD, xyza) => Ax, By, Cz, Da
+        grads_and_var = list(zip(grads, var))
 
         self.grads = grads
         self.var = var
@@ -171,10 +189,11 @@ class Model(object):
 
         self.train_model = train_model
         self.act_model = act_model
-        self.step = act_model.step
+        self.step = act_model.step  # TODO: remove
         self.value = act_model.value
-        self.initial_state = act_model.initial_state
+        self.initial_state = act_model.initial_state  # TODO: remove
 
+        # TODO: move to separate methods
         self.save = functools.partial(save_variables, sess=sess)
         self.load = functools.partial(load_variables, sess=sess)
 
@@ -206,21 +225,18 @@ class Model(object):
             (batch_size, obs_dim) matrix of observations
         returns : array_like
             (batch_size,) vector of returns
-        masks : TODO
-            TODO
-        actions : TODO
+        actions : array_like
             (batch_size, ac_dim) matrix of actions
         values : array_like
             (batch_size,) vector of values
-        neglogpacs : TODO
-            TODO
-        states : TODO
-            TODO
+        neglogpacs : array_like
+            negative log probabilities of the old policy TODO size
 
         Returns
         -------
-        TODO
-            TODO
+        list of float  TODO: array_like? try printing
+            the values associated with the individual elements of the loss
+            function
         """
         # Here we calculate advantage A(s,a) = R + yV(s') - V(s)
         # Returns = R + yV(s')
@@ -230,20 +246,17 @@ class Model(object):
         advs = (advs - advs.mean()) / (advs.std() + 1e-8)
 
         td_map = {
-            self.train_model.X: obs,
+            self.obs_ph: obs,
             self.ac_ph: actions,
             self.adv_ph: advs,
             self.ret_ph: returns,
             self.learning_rate: lr,
             self.clip_range: clip_range,
-            self.OLDNEGLOGPAC: neglogpacs,
-            self.OLDVPRED: values
+            self.old_neglogpac: neglogpacs,
+            self.old_vpred: values
         }
         if states is not None:
             td_map[self.train_model.S] = states
             td_map[self.train_model.M] = masks
 
-        return self.sess.run(
-            self.stats_list + [self._train_op],
-            td_map
-        )[:-1]
+        return self.sess.run(self.stats_list + [self._train_op], td_map)[:-1]
